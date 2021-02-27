a21006fc511dacb344bcc8d10e3407c306ad147d
Wayne Van Son <waynevanson@gmail.com>
2021-02-27 19:40:58 +1100

    fake: fake
    BREAKING CHANGE: fake

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,3 @@ export * as readerIOOption from "./reader-io-option"
 export * as utilities from "./util"
 export * as meta from "./meta"
 export * as readerIOEither from "./reader-io-either"
-const check = 2
