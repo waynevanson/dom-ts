19bbb4b641788ae6bcaea7eccba3dc24c51e59d1
Wayne Van Son <waynevanson@gmail.com>
2021-02-27 19:32:26 +1100

    feat(fake): not a real commit
    BREAKING CHANGE: dummy release

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * as readerIOOption from "./reader-io-option"
 export * as utilities from "./util"
 export * as meta from "./meta"
 export * as readerIOEither from "./reader-io-either"
+const check = 2
